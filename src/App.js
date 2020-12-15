2c04c45145d7fabacc701d6b5fd15160cfa488f4
Manuel Resinas <resinas@us.es>
2020-12-15 09:28:59 +0000

    First steps with contacts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Contact from './Contact.js';
 
 function App() {
+
+  const c = {
+    name: "Alexandera",
+    phone: "666777888"
+  }
+
   return (
     <div className="App">
       <h1>Contacts App</h1>
+      <Contact contact={c}/>
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+
+function Contact(props) {
+    return (
+        <div>
+            <div>Name: {props.contact.name}</div>
+            <div>Phone: {props.contact.phone}</div>
+        </div>
+    )
+}
+
+export default Contact;
